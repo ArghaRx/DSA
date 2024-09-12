24ee2c41168bb566422bc1bec3b4e335178e3191
Argha Ghosh <arghag889@gmail.com>
2024-09-12 12:42:00 +0530

    12nd sep,24 POTD

---
+++ String.java/1684. Count the Number of Consistent Strings.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/count-the-number-of-consistent-strings/description/
+
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : allowed.toCharArray())
+        map.put(c, map.getOrDefault(c, 0) + 1);
+
+        int cnt = 0;
+        for(String str : words)
+        {
+            boolean flag = true;
+            for(char c : str.toCharArray())
+            {
+                if(!map.containsKey(c)) 
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true) cnt++;
+        }
+        return cnt;
+    }
+}
