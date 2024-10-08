5d48c7626da99e952ebd188ced186569c64c853f
Argha Ghosh <141269702+ArghaRx@users.noreply.github.com>
2024-10-08 08:38:59 +0530

    8th oct 2024 POTD

---
+++ String.java/1963. Minimum Number of Swaps to Make the String Balanced.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/description/
+
+class Solution {
+    public int minSwaps(String s) {
+        int count = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == '[')
+            count++;
+
+            else if(count > 0)
+            count--;
+        }
+        return (count + 1) / 2;
+    }
+}
