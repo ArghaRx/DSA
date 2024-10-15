1bcd98dfa12b5798212c4e7964d0097ac5e8419e
Argha Ghosh <141269702+ArghaRx@users.noreply.github.com>
2024-10-15 11:28:45 +0530

    15th oct 2024 POTD

---
+++ String.java/2938. Separate Black and White Balls.java
@@ -0,0 +1,14 @@
+//  https://leetcode.com/problems/separate-black-and-white-balls/description/
+
+class Solution {
+    public long minimumSteps(String s) {
+        long swap = 0;
+        long black = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '0') swap += black;
+            else black++;
+        }
+        return swap;
+    }
+}
