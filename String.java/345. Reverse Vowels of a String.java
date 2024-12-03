0ea8df55b5e96520f0b732b05d0c5ef1134887e8
Argha Ghosh <141269702+ArghaRx@users.noreply.github.com>
2024-12-03 11:18:19 +0530

    V.IMP

---
+++ String.java/345. Reverse Vowels of a String.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public String reverseVowels(String s) {
+        // Define a set of vowels for quick lookup
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
+        
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            while (left < right && !vowels.contains(chars[left])) {
+                left++;
+            }
+            while (left < right && !vowels.contains(chars[right])) {
+                right--;
+            }
+          // after getting out from both while loop, left and right both indicate vowels char
+          // so swap them
+            if (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        return new String(chars);
+    }
+}
